c7522530f7a172af30b2ddfa6c6ab35c94b5c6b4
Avonde <romain@avonde.email>
2019-09-25 21:49:06 +0200

    [ExempleBot] Passage de l'extension de conversion du csproj

--- Poker.ExempleBot/Properties/AssemblyInfo.cs
+++ Poker.ExempleBot/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("Poker.ExempleBot")]
-[assembly: AssemblyDescription("Exemple de bot pour le Poker en réseau")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Pat & Pico")]
-[assembly: AssemblyProduct("Poker.ExempleBot")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
@@ -21,15 +9,3 @@ using System.Runtime.InteropServices;
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("d305bdfe-98f2-4e47-b293-70bfa275d60b")]
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("4.3.*")]
-[assembly: AssemblyFileVersion("4.3.0.0")]
