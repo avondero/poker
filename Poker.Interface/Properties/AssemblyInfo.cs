dc18b01682f8d0930b55216f1939e91c27f2dcdc
Avonde <romain@avonde.email>
2019-09-25 21:37:58 +0200

    Passage de l'extension de conversion

--- Poker.Interface/Properties/AssemblyInfo.cs
+++ Poker.Interface/Properties/AssemblyInfo.cs
@@ -2,31 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("Poker.Interface")]
-[assembly: AssemblyDescription("Jeu de poker en réseau")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Pat & Pico")]
-[assembly: AssemblyProduct("PokerEnReseau")]
-[assembly: AssemblyCopyright("Copyright @  2007")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("4.3.*")]
-[assembly: AssemblyFileVersion("4.3.0.0")]
